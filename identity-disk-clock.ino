fceade86fb3dba9e1d9caf08d1eceaf1762a32dd
Jarek Lupinski <jarek319@gmail.com>
2018-07-05 20:01:36 -0400

    work in progress

---
+++ identity-disk-clock.ino
@@ -0,0 +1,24 @@
+int CLK = 13;
+int SER = 11;
+int LAT = 10;
+
+void setup() {
+  pinMode(CLK, OUTPUT);
+  pinMode(SER, OUTPUT);
+  pinMode(LAT, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LAT, LOW);
+  shiftOut(SER, CLK, MSBFIRST, 0b11110111);
+  digitalWrite(LAT, HIGH);
+  
+  delayMicroseconds(250);
+  /*
+  digitalWrite(LAT, LOW);
+  shiftOut(SER, CLK, MSBFIRST, 0b11111111);
+  digitalWrite(LAT, HIGH);
+  
+  delayMicroseconds(250);
+  */
+}
